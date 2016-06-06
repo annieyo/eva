d35df68368faa6eff575f4bb468b04a47224043a
Lila <yersinianb@gmail.com>
2016-06-06 14:00:29 +0200

    Gerüst für'n ldap ds

---
+++ ldapProve.php
@@ -0,0 +1,10 @@
+<?php
+$username = "ds";
+$password = "12345";
+
+//ldap-Port wird beim Bind automatisch geadded
+$ldap = ldap_connect("imlinkoo.de");
+//geht nur bei Lehrerkürzeln.  Pfad für Schüler ist anders.
+if($bind=ldab_bind($ldap, "uid=".$username.",ou=lehrer,ou=benutzer,dc=gso-neu", $password)){echo "klappt";}
+else echo "nö";
+?>
